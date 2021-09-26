cfdccb31ed345f8bb9f2971d61dad432e3a51666
Thiago Gomes <thiago123789@gmail.com>
2021-09-25 21:54:04 -0300

    update sh and yaml files

---
+++ kubernetes/deploy-kubernetes.sh
@@ -0,0 +1,30 @@
+#!/bin/zsh
+
+echo "deleting previous version"
+kubectl delete svc mysql-subscription-svc
+kubectl delete deployment mysql-subscription-deploy
+kubectl delete svc subscription-svc
+kubectl delete deployment subscription-deployment
+kubectl delete svc bff-svc
+kubectl delete deployment bff-deployment
+
+echo "apply kubernetes secrets"
+kubectl apply -f ./secrets.yaml
+
+echo "apply mysql deployment"
+kubectl apply -f ./mysql-deployment.yaml
+
+echo "apply mysql service"
+kubectl apply -f ./mysql-service.yaml
+
+echo "apply backend deployment"
+kubectl apply -f ./backend.yaml
+
+echo "apply backend service"
+kubectl apply -f ./backend-svc.yaml
+
+echo "apply bff deployment"
+kubectl apply -f ./bff-deployment.yaml
+
+echo "apply bff service"
+kubectl apply -f ./bff-svc.yaml
