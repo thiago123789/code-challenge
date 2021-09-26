33627932ba2e12a15fb884ccf5f09e85d3e9e3fd
Thiago Gomes <thiago123789@gmail.com>
2021-09-26 10:27:28 -0300

    update email tests

--- email/src/test/java/code/challenge/email/EmailApplicationTests.java
+++ email/src/test/java/code/challenge/email/EmailApplicationTests.java
@@ -3,7 +3,6 @@ package code.challenge.email;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class EmailApplicationTests {
 
     @Test
